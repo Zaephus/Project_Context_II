2fc25780cd50ad81dd66195fd466d8a4e812ed14
Marc Rozendaal <rozendaalmarc@gmail.com>
2023-02-26 02:26:08 +0100

    Added resizable terrain generation

--- Context II/Assets/01_Scripts/LevelEditor/LevelEditor.cs
+++ Context II/Assets/01_Scripts/LevelEditor/LevelEditor.cs
@@ -1,20 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class LevelEditor : MonoBehaviour {
 
+    private int size = 5;
+
     [SerializeField]
-    private int size;
+    private Slider slider;
+
+    [SerializeField]
+    private TMP_Text sizeValueText;
 
     private TerrainGenerator terrainGenerator;
 
+    private List<Tile> tiles = new List<Tile>();
+
     private void Start() {
-        if(size % 2 == 0) {
-            size++;
-        }
+
         terrainGenerator = GetComponent<TerrainGenerator>();
-        terrainGenerator.Generate(size);
+        SizeValueChanged();
+
+    }
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.D)) {
+            slider.value ++;
+        }
+        if(Input.GetKeyDown(KeyCode.A)) {
+            slider.value --;
+        }
+    }
+
+    public void SizeValueChanged() {
+
+        if(slider.value % 2 == 0) {
+            slider.SetValueWithoutNotify(slider.value + 1);
+        }
+
+        size = Mathf.RoundToInt(slider.value);
+        sizeValueText.text = size.ToString();
+        
+        tiles = terrainGenerator.Generate(size, tiles);
+
     }
 
 }
--- Context II/Assets/01_Scripts/LevelEditor/TerrainGenerator.cs
+++ Context II/Assets/01_Scripts/LevelEditor/TerrainGenerator.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class TerrainGenerator : MonoBehaviour {
@@ -15,8 +16,13 @@ public class TerrainGenerator : MonoBehaviour {
         Vector3 tilePos;
         Vector3Int hexPos;
 
-        GameObject objectToInstantiate;
-        TileType type;
+        GameObject objectToInstantiate = TileDatabase.Instance.GetTileByType(TileType.BaseTile);
+        TileType type = TileType.BaseTile;
+
+        int previousSize = 0;
+        if(_existingTiles != null && _existingTiles.Count > 0) {
+            previousSize = GetHexDistanceInt(Vector3.zero, _existingTiles[0].hexPosition) * 2 + 1;
+        }
 
         int yMin;
         int yMax;
@@ -27,58 +33,107 @@ public class TerrainGenerator : MonoBehaviour {
         int r;
         int s;
 
-        // int previousSize;
+        if(_size >= previousSize) {
+            for(int x = -_size/2; x <= _size/2; x++) {
 
-        // if(_existingTiles != null) {
-        //     for(int i = 0; i < _existingTiles.Count; i++) {
-        //         if(_existingTiles[i].hexPosition.y)
-        //     }
-        // }
-
-        for(int x = -_size/2; x <= _size/2; x++) {
-
-            if(x >= 0) {
-                yMin = x - _size/2;
-                yMax = _size/2;
-            }
-            else {
-                yMin = -_size/2;
-                yMax = _size/2 + x;
-            }
-
-            for(int y = yMin; y <= yMax; y++) {
-
-                if(y != 0) {
-                    xOffset = y * 0.5f;
+                if(x >= 0) {
+                    yMin = x - _size/2;
+                    yMax = _size/2;
                 }
                 else {
-                    xOffset = 0.0f;
+                    yMin = -_size/2;
+                    yMax = _size/2 + x;
                 }
 
-                q = x;
-                r = -y;
-                s = -q - r;
+                for(int y = yMin; y <= yMax; y++) {
 
-                tilePos = new Vector3((x - xOffset) * hexWidth, 0, y * 0.75f * hexHeight);
-                hexPos = new Vector3Int(q, r, s);
+                    q = x;
+                    r = -y;
+                    s = -q - r;
 
-                objectToInstantiate = TileDatabase.Instance.GetTileByType(TileType.BaseTile);
-                type = TileType.BaseTile;
+                    hexPos = new Vector3Int(q, r, s);
 
-                Tile tile = Instantiate(objectToInstantiate, tilePos, objectToInstantiate.transform.rotation, transform).GetComponent<Tile>();
+                    if(_existingTiles != null) {
+                        Tile currentTile = _existingTiles.Find(tile => tile.hexPosition == hexPos);
+                        if(currentTile != null) {
+                            tiles.Add(currentTile);
+                            continue;
+                        }
+                    }
+                        
+                    if(y != 0) {
+                        xOffset = y * 0.5f;
+                    }
+                    else {
+                        xOffset = 0.0f;
+                    }
 
-                tile.hexPosition = hexPos;
-                tile.tileRotation = TileRotation.Zero;
-                tile.tileHeight = TileHeight.Zero;
-                tile.tileType = type;
-                tiles.Add(tile);
+                    tilePos = new Vector3((x - xOffset) * hexWidth, 0, y * 0.75f * hexHeight);
+
+                    objectToInstantiate = TileDatabase.Instance.GetTileByType(TileType.BaseTile);;
+
+                    Tile tile = Instantiate(objectToInstantiate, tilePos, objectToInstantiate.transform.rotation, transform).GetComponent<Tile>();
+
+                    tile.hexPosition = hexPos;
+                    tile.tileRotation = TileRotation.Zero;
+                    tile.tileHeight = TileHeight.Zero;
+                    tile.tileType = type;
+                    tiles.Add(tile);
+
+                }
 
             }
-
         }
+        else if(_size < previousSize) {
 
+            int yMinNew;
+            int yMaxNew;
+
+            for(int x = -previousSize/2; x <= previousSize/2; x++) {
+
+                if(x >= 0) {
+                    yMin = x - previousSize/2;
+                    yMax = previousSize/2;
+                    yMinNew = x - _size/2;
+                    yMaxNew = _size/2;
+                }
+                else {
+                    yMin = -previousSize/2;
+                    yMax = previousSize/2 + x;
+                    yMinNew = -_size/2;
+                    yMaxNew = _size/2 + x;
+                }
+
+                for(int y = yMin; y <= yMax; y++) {
+
+                    q = x;
+                    r = -y;
+                    s = -q - r;
+
+                    hexPos = new Vector3Int(q, r, s);
+
+                    Tile currentTile = _existingTiles.Find(tile => tile.hexPosition == hexPos);
+
+                    if(GetHexDistanceInt(Vector3.zero, hexPos) * 2 + 1 <= _size) {
+                        tiles.Add(currentTile);
+                    }
+                    else {
+                        Destroy(currentTile.gameObject);
+                    }
+
+                }
+
+            }
+            
+        }
+        
         return tiles;
 
+    }
+
+    private int GetHexDistanceInt(Vector3 _posOne, Vector3 _posTwo) {
+        Vector3 diff = _posOne - _posTwo;
+        return (int)Mathf.Max(Mathf.Abs(diff.x), Mathf.Abs(diff.y), Mathf.Abs(diff.z));
     }
     
 }

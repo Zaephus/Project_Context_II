8baf673c06863bd0062d94d75e6fabb85affd69d
FabianMvanDorst <63244460+FabianMvanDorst@users.noreply.github.com>
2023-03-04 21:06:34 +0100

    Approval + mainmenu + wind + zoom
    stuff

--- Context II/Assets/01_Scripts/LevelEditor/CameraMovement.cs
+++ Context II/Assets/01_Scripts/LevelEditor/CameraMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour {
 
+    public Camera m_Camera;
+
     [SerializeField]
     private float moveSpeed;
 
@@ -16,10 +18,20 @@ public class CameraMovement : MonoBehaviour {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
+
         Vector3 velocity = new Vector3(horizontal, 0, vertical);
         velocity.Normalize();
 
         transform.position += velocity * moveSpeed * Time.deltaTime;
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f && m_Camera.orthographicSize <= 20) // zoom in
+        {
+            m_Camera.orthographicSize++;
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") > 0f && m_Camera.orthographicSize >= 2)  // zoom out
+        {
+            m_Camera.orthographicSize--;
+        };
     }
 
 }
